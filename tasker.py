82eaff6c6e117354f837f76e54894bf35eae896d
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2019-03-09 15:15:13 +0200

    Keep tasker alive after error
    lazily, for now

--- tasker.py
+++ tasker.py
@@ -10,13 +10,16 @@ async def tasker():
     print("Starting tasker..")
     while True:
         print("Doing tasks..")
-        records = await db.get_all_player_guids()
-        for record in records:
-            await main.check_for_elo_change(record['player_guid'])
-            await main.check_and_handle_nickname_change(record['player_guid'])
-            await main.check_for_new_matches(record['player_guid'])
-        print("Tasks done.")
-        await asyncio.sleep(CHECK_INTERVAL)
+        try:
+            records = await db.get_all_player_guids()
+            for record in records:
+                await main.check_for_elo_change(record['player_guid'])
+                await main.check_and_handle_nickname_change(record['player_guid'])
+                await main.check_for_new_matches(record['player_guid'])
+            print("Tasks done.")
+            await asyncio.sleep(CHECK_INTERVAL)
+        except:
+            continue
 
 
 if __name__ == "__main__":
